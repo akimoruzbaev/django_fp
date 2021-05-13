d609b03fb6dd70ebb72f3ac0b9a8e154c69ccc2c
Bekkazy <bekkazy.k@gmail.com>
2021-05-13 21:26:57 +0600

    New html

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
+    clipped_text = models.TextField(max_length=150)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now=True)
     views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'

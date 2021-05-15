9d316faf75e74cc305413fa8f6cc2e57de793243
Bekkazy <bekkazy.k@gmail.com>
2021-05-15 21:11:59 +0600

    Forms

---
+++ blog/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+attrsFirst = {
+    'class': "form-control",
+    'style': "margin-bottom: -1px;border-bottom-right-radius: 0;border-bottom-left-radius: 0;"
+}
+
+attrsLast = {
+    'class': "form-control",
+    'style': "margin-bottom: 10px;border-top-left-radius: 0;border-top-right-radius: 0;"
+}
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label='Username',
+        max_length=100,
+        widget=forms.TextInput(attrs=attrsFirst))
+    password = forms.CharField(
+        label='Password',
+        max_length=64,
+        widget=forms.PasswordInput(attrs=attrsLast))

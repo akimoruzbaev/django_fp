06b276971c271012486c0314552ad808f5c80d73
Bekkazy <bekkazy.k@gmail.com>
2021-05-20 21:00:42 +0600

    url changed

--- blog/urls.py
+++ blog/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     path('register', views.reqister, name='register'),
     path('login', views.loginView, name='login'),
     path('register', views.reqister, name='register'),
-#     path('add_post', views.add_post, name='add_post'), # Arli
-#     path('user/<int:pk>/', views.user, name='user'), # Bakdoolot
+    # path('landing', views.landing, name='landing'), # Daniyar
+    #path('add_post', views.add_post, name='add_post'), # Arli
+    #path('user/<int:pk>/', views.user, name='user'), # Bakdoolot
     path('search', views.search, name='search'),  # Ainura
-#     path('contact', views.contact, name='contact'), # Askat
-#     path('404', views.not_found, name='not_found') # Akim
- ]
+    #path('contact', views.contact, name='contact'), # Askat
+    #path('404', views.not_found, name='not_found') # Akim
+]
